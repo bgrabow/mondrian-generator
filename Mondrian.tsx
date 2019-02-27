6cfba3853dff7d951a097c6d0cc4bd8914f97b21
ben <ben.grabow@pm.me>
2019-02-27 16:38:49 -0600

    Finished with Mondrian spike

--- Mondrian.tsx
+++ Mondrian.tsx
@@ -1,21 +1,8 @@
 import { Component } from "react";
-import { View, ViewStyle } from "react-native";
-import { FlexDirectionProperty } from "csstype";
-
-function hue(h: number) {
-    return `hsl(${0},100%,50%)`
-}
+import { View, StyleSheet } from "react-native";
 
 function last(coll: ArrayLike<any>) {
     return coll[coll.length - 1];
-}
-
-function reductions<A, E>(coll: ArrayLike<E>, init: A, f: (acc: A,e: E) => A): Array<A> {
-    let rxs = [init];
-    for (let i = 0; i < coll.length; i++) {
-        rxs.push(f(last(rxs), coll[i]));
-    }
-    return rxs;
 }
 
 function weightedRandomIndex(weights: Array<number>) {
@@ -26,39 +13,121 @@ function weightedRandomIndex(weights: Array<number>) {
 };
 
 function mondrianColorPalette() {
-    const redYellowBlue = ["#FFF","#F00","#FF0","#00F", "#000", "#AAA"];
+    const redYellowBlue = [
+        "#FFF",
+        "#F00",
+        "#FF0",
+        "#00F",
+        "#000",
+        "#AAA"];
     const hueBasis = Math.floor(Math.random() * 360);
-    const hslRotated = ["#FFF",hue(hueBasis),hue(hueBasis + 60),hue(hueBasis + 240), "#000", "#AAA"];
+    const hslRotated = [
+        "#FFF",
+        `hsl(${hueBasis},100%,50%)`,
+        `hsl(${hueBasis+60},100%,50%)`,
+        `hsl(${hueBasis+240},100%,50%)`,
+        "#000",
+        "#AAA"];
     const weights = [12, 14, 16, 18, 19, 20];
-    const randomPalette = [redYellowBlue, hslRotated][Math.floor(Math.random() * 2)];
+    const randomPalette = randomElement([/* redYellowBlue, */ hslRotated]);
     return () => { return randomPalette[weightedRandomIndex(weights)]; };
 }
 
-function generateMondrian(maxDepth: number, complexity: number, orientation: FlexDirectionProperty) {
-    const divisions = maxDepth == 0 ? 0 : Math.floor(Math.random() * complexity);
+function childOrientation(orientation: Orientation) {
+    return orientation == "column" ? "row" : "column";
 }
 
-interface MondrianProps {
-    divisions: number,
-    cellStyle: ViewStyle,
-    maxDepth: number,
+function randomElement(coll: Array<any>) {
+    return coll[Math.floor(Math.random() * coll.length)];
+}
+
+function generateMondrian(
     complexity: number,
-    orientation: FlexDirectionProperty,
-    childOrientation: FlexDirectionProperty
-}
+    sizes: Array<number>,
+    colorPalette: () => string,
+    maxDepth: number,
+    orientation: Orientation) {
 
-class Mondrian extends Component<MondrianProps> {
-    render() {
-        if (this.props.divisions == 0) {
+    const divisions = maxDepth == 0 ? 0 : Math.floor(Math.random() * complexity);
+    switch (divisions) {
+        case 0:
             return (
-                <View style={this.props.cellStyle}></View>
+                <MondrianCell colorPalette={colorPalette}/>
             );
-        } else {
+        default:
             return (
-                <View></View>
-            );
-        }
+                <MondrianBody
+                    colorPalette={colorPalette}
+                    maxDepth={maxDepth}
+                    sizes={sizes}
+                    flexGrow={randomElement(sizes)}
+                    divisions={divisions}
+                    complexity={complexity}
+                    orientation={orientation}
+                />);
     }
 }
 
-export {mondrianColorPalette, generateMondrian, weightedRandomIndex};
+type Orientation = "column" | "row" | "row-reverse" | "column-reverse" | undefined;
+
+interface MondrianBodyProps {
+    colorPalette: () => string,
+    maxDepth: number,
+    sizes: Array<number>,
+    flexGrow: number,
+    divisions: number,
+    complexity: number,
+    orientation: Orientation
+}
+
+function forTimes<T>(n: number, f: (n: number)=>T):Array<T> {
+    let xs=[];
+    for(let i=0; i<n; i++) {
+        xs.push(f(i));
+    }
+    return xs;
+}
+
+interface MondrianCellProps {
+    colorPalette: () => string
+}
+
+class MondrianCell extends Component<MondrianCellProps> {
+    render() {
+        const style = {
+            flex: 1,
+            borderWidth: 6,
+            borderColor: "#000",
+            backgroundColor: this.props.colorPalette()
+        }
+        return(<View style={style}/>);
+    }
+}
+
+class MondrianBody extends Component<MondrianBodyProps> {
+    render() {
+        const styles = StyleSheet.create({
+            bodyStyle: {
+                flex: this.props.flexGrow,
+                flexDirection: this.props.orientation
+            }
+        });
+        return (
+            <View style={styles.bodyStyle}>
+                {forTimes(this.props.divisions,(i: number)=>{
+                    Object.assign(
+                        generateMondrian(
+                            this.props.complexity,
+                            this.props.sizes,
+                            this.props.colorPalette,
+                            this.props.maxDepth - 1,
+                            childOrientation(this.props.orientation)
+                            ),
+                        {key: i})
+                })}
+            </View>
+        );
+    }
+}
+
+export { mondrianColorPalette, generateMondrian, weightedRandomIndex };
